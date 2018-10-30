8c94c01c12210b57363328e2b82624a708d32aee
lijinghuan <1072090332@qq.com>
2018-10-30 10:35:27 +0800

    test build

---
+++ precache-manifest.92ee0f616ee752a30bfd1c3e13b3c1b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "cc53bb2ae15b28b787db",
+    "url": "./static/js/main.cc53bb2a.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "./static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "cc53bb2ae15b28b787db",
+    "url": "./static/css/main.de012d24.chunk.css"
+  },
+  {
+    "revision": "b0a3fe562193f3a7886c829b0f4f9943",
+    "url": "./index.html"
+  }
+];
